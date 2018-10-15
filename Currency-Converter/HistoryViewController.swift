7e6f87a1f0170fc31a5dc99b5d4c975aaf0f34ab
Amin Amjadi <ma.amjadi@icloud.com>
2018-10-15 14:14:29 +0200

    test cases have been added, data loads to the chart

--- Currency-Converter/HistoryViewController.swift
+++ Currency-Converter/HistoryViewController.swift
@@ -13,6 +13,12 @@ class HistoryViewController: UIViewController {
     
     @IBOutlet weak var chartView: LineChartView!
     
+    private var chartData = [(key: String, value: Float)]() {
+        didSet {
+            setChartData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,11 +28,7 @@ class HistoryViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         RateHandler.shared.convertionForLastSevenDays { (succ, err, data) in
-            if !(data.isEmpty) {
-                for theData in data {
-                    print(theData)
-                }
-            }
+            self.chartData = data
         }
     }
     
@@ -36,7 +38,42 @@ class HistoryViewController: UIViewController {
         chartView.dragEnabled = true
         chartView.setScaleEnabled(true)
         chartView.pinchZoomEnabled = true
+        chartView.legend.form = .line
         chartView.animate(xAxisDuration: 2.5)
+    }
+    
+    private func setChartData() {
+        let values = (0..<chartData.count).map { (i) -> ChartDataEntry in
+            let val = chartData[i].value
+            return ChartDataEntry(x: Double(i), y: Double(val))
+        }
+        
+        let set1 = LineChartDataSet(values: values, label: "DataSet 1")
+        set1.drawIconsEnabled = false
+        
+        set1.lineDashLengths = [5, 2.5]
+        set1.highlightLineDashLengths = [5, 2.5]
+        set1.setColor(.black)
+        set1.setCircleColor(.black)
+        set1.lineWidth = 1
+        set1.circleRadius = 3
+        set1.drawCircleHoleEnabled = false
+        set1.valueFont = .systemFont(ofSize: 9)
+        set1.formLineDashLengths = [5, 2.5]
+        set1.formLineWidth = 1
+        set1.formSize = 15
+        
+        let gradientColors = [UIColor(named: "LightColor")?.withAlphaComponent(0.1).cgColor,
+                              UIColor(named: "LightColor")?.cgColor]
+        let gradient = CGGradient(colorsSpace: nil, colors: gradientColors as CFArray, locations: nil)!
+        
+        set1.fillAlpha = 1
+        set1.fill = Fill(linearGradient: gradient, angle: 90) //.linearGradient(gradient, angle: 90)
+        set1.drawFilledEnabled = true
+        
+        let data = LineChartData(dataSet: set1)
+        
+        chartView.data = data
     }
 
 }
